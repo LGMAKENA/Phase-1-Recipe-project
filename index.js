0d4e8baa80e6d63727b00755bdf4d3f35eaf8661
lgmakena <linetmakenag@gmail.com>
2023-04-27 12:02:55 +0300

    changed from local host to live url

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const recipeList = document.querySelector('#recipe-items');
 const mainContent = document.querySelector('.main-content');
 
 async function loadRecipes() {
-  const response = await fetch('http://localhost:3000/recipes');
+  // const response = await fetch('http://localhost:3000/recipes');
+  const response = await fetch('https://recipes-g6go.onrender.com/recipes');
+
   const data = await response.json();
   return data;
 }
