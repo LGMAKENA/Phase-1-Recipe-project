fd4a6b308851dcd4588e9cdd68bebf5f3734c4a6
lgmakena <linetmakenag@gmail.com>
2023-04-27 09:39:50 +0300

    adding recipe list and images

--- index.js
+++ index.js
@@ -6,3 +6,39 @@ async function loadRecipes() {
   const data = await response.json();
   return data;
 }
+//aside data binding
+function renderRecipes(recipe) {
+    recipeList.innerHTML = '';
+    recipe
+      .map(item => {
+        return {
+          title: item.title,
+          image: item.image,
+          likes: item.aggregateLikes,
+        };
+      })
+      .forEach(food => {
+        const li = document.createElement('li');
+        const div = document.createElement('div');
+        div.classList.add('recipe');
+        const img = document.createElement('img');
+        img.src = food.image;
+        img.alt = food.alt;
+        div.append(img);
+        const div2 = document.createElement('div');
+        div2.classList.add('recipe-info');
+        const p = document.createElement('p');
+        p.classList.add('recipe-name');
+        p.innerText = food.title;
+        div2.append(p);
+        div.append(img);
+        div.append(div2);
+        li.append(div);
+        recipeList.append(li);
+        li.addEventListener('click', e => {
+          const selectedTitle =
+            e.target.querySelector('.recipe-name').textContent;
+          bindRecipeInfo(selectedTitle);
+        });
+      });
+  }
